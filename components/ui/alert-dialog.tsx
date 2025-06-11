c567d22026f8ada830b32e2546ce2886f91c43ca
Shaman <142025829+shamanshetty@users.noreply.github.com>
2025-06-11 21:38:43 +0530

    Update alert-dialog.tsx

--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -8,6 +8,7 @@ import { buttonVariants } from "@/components/ui/button"
 
 const AlertDialog = AlertDialogPrimitive.Root
 
+
 const AlertDialogTrigger = AlertDialogPrimitive.Trigger
 
 const AlertDialogPortal = AlertDialogPrimitive.Portal
